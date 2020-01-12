02942444efbfea54743fbf85fcebeba209336583
Mike <flanman1866@gmail.com>
2020-01-12 00:48:34 -0700

    statistics and yearly index dictionary

--- initial_data_viewing.py
+++ initial_data_viewing.py
@@ -10,66 +10,90 @@ filename = 'EURUSD_GMT_ts_bid_ask_5-4-02--9-9-19.csv' # this is like 11 GB so be
 
 ##data = pd.read_csv(filename, delimiter=',',skiprows=0)
 
-# to deal with me having add a header once...
-##try: ts = data['Date'].str.split(" ",n=1, expand=True)
-##except KeyError:
-##    data = pd.read_csv(filename, delimiter=',',skiprows=0,names=['Date','Bid','Ask'])
-##    ts = data['Date'].str.split(" ",n=1, expand=True)
+Num_ticks = 100
+Time_forcast = 5*60 # seconds
+Pip_thresh = 20
+
+def time_parse(time_string):
+    nofrag = time_string.split('.')[0:-1]
+    nofrag='.'.join(nofrag)
+    frag = time_string.split('.')[-1]
     
-##data['Day']=ts[0]
-##data['Time']=ts[1]
-##data.drop(columns=['Date'],inplace=True)
-##data['Day'] = pd.to_datetime(data['Day'],yearfirst=True) # parse days into date time frmt
-##
-##ts_day = data['Day']
-##bid = data['Bid']
-##ask = data['Ask']
+    nofrag_dt = dt.datetime.strptime(nofrag,'%Y.%m.%d %H:%M:%S')
+    dt_time = nofrag_dt.replace(microsecond=int(frag)*1000)
+    return dt_time
+    
+    
+year_indices = {}
 
-##rng = pd.date_range(datetime.date(2015,1,1),datetime.date(2015,7,31))
+def process_data(dat, Nticks, Tforcast, Pthresh):
+    global year_indices
+    
+    start_time = time_parse(dat.loc[dat.index[0],'Date'])
+    end_time = time_parse(dat.loc[dat.index[-1],'Date'])
+    elapsed_time = end_time - start_time
+    
+    if str(start_time.year) not in year_indices.keys():
+        year_indices[str(start_time.year)] = dat.index[0]
+        print(year_indices)
+    # ~ dt_list = []
+    # ~ for row in dat.index:
+        # ~ dt_list.append(time_parse(dat.loc[row,'Date']))
+    
+    # ~ dat['dt_obj'] = dt_list
+        
+    # ~ print('start year: ',start_time.year,' month: ',start_time.month,' day: ',start_time.day)
+    # ~ print('elapsed:',elapsed_time)
+    
+    # ~ print('Start Bid',dat.loc[dat.index[0],'Bid'])
+    # ~ print('Start Date',dat.loc[dat.index[0],'Date'])
+    # ~ print('End Date',dat.loc[dat.index[-1],'Date'])
+    
+    pip_range = (max(dat['Bid'].values)-min(dat['Bid'].values))*10000
+    # ~ print('Range of Bid variation:',pip_range)
+    
+    
+    
+    return pip_range > 20 # nothing for now
 
-### hacky
-##freq = []
-##for yy in range(2003,2020):
-##    print('counting data in year ',yy,' ...')
-##    for mm in range(1,13): # i think one too big
-##        for dd in range(1,33): # hacky
-##            try:
-##                freq.append(data.loc[data['Day'] == pd.Timestamp(datetime.date(yy,mm,dd))].shape[0])
-##            except ValueError: pass
-
-##plt.plot(range(len(bid.values)),bid.values)
-##plt.show()
-
-##ticks_per_day = data['Day'].value_counts()
-
-##fig, ax = plt.subplots()
-
-##ax.set_xticks(data['Day'])
-##ax.xaxis_date()
-
-##ax.xaxis.set_major_formatter(mdates.DateFormatter("%Y-%m-%d"))
-##ax.xaxis.set_minor_formatter(mdates.DateFormatter("%Y-%m-%d"))
-##plt.xticks(rotation=90)
-##ax = data['Day'].value_counts().plot(kind='bar')
-##ax.bar(data['Day'].values,data['Day'].value_counts().values)
-##plt.show()
-
-
-##chunksize = 10 ** 8
-chunksize = 1000
-c = 0
-timer = dt.datetime.now()
-# each chunk really is a new dataframe
-for chunk in pd.read_csv(filename, chunksize=chunksize, names=['Date','Bid','Ask']):
-    ems = (dt.datetime.now() - timer).microseconds
+try:
+    ##chunksize = 10 ** 8
+    chunksize = 1000
+    c = 0
     timer = dt.datetime.now()
-    print('micros:',ems)
-    print(' ')
-##    print('chunk ',c)
-##    print(chunk)
-##    process(chunk)
-    if c > 2: break
-    c += 1
+    init_timer = dt.datetime.now()
+    # each chunk really is a new dataframe
+    # skiprows = 100000000 gets to around 2010
+    # skiprows=200000000 around 2016
+    for chunk in pd.read_csv(filename, skiprows=0, chunksize=chunksize, names=['Date','Bid','Ask']):
+        ems = (dt.datetime.now() - timer).microseconds # ems = elapsed microseconds
+        timer = dt.datetime.now()
+        print('micros:',ems,' total: ',(dt.datetime.now()-init_timer))
+        print(' ')
+        
+    ##    print('chunk ',c)
+        # ~ print(chunk)
+        
+        flag = process_data(chunk, Num_ticks, Time_forcast, Pip_thresh)
+        
+        # ~ if flag: 
+        # ~ bids = chunk['Bid'].values
+        # ~ plt.clf()
+        # ~ plt.plot(bids)
+        # ~ plt.show()
+        # ~ plt.pause(0.75)
+        #if c > 300000: break
+        c += 1
+except KeyboardInterrupt: pass
 
+print(year_indices)
+
+# ~ import pickle
+# ~ f = open("year_inds.pkl","wb")
+# ~ pickle.dump(year_indices,f)
+# ~ f.close()
+
+# ~ pkl_file = open('year_inds.pkl', 'rb')
+# ~ file = pickle.load(pkl_file)
 
 ##df.to_csv('my_csv.csv', mode='a', header=False)
