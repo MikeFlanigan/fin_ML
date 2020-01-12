5009538aeb1047d5794ce4cf7c20144b6dad8299
flanman1866 <flanman1866@gmail.com>
2020-01-11 17:19:37 -0700

    starting to set up data processing in chunks

--- initial_data_viewing.py
+++ initial_data_viewing.py
@@ -1,27 +1,29 @@
 import numpy as np
 import pandas as pd
-import datetime
+import datetime as dt
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 
 ##filename = 'EURUSD_GMT+0_NO-DST.csv'
-filename = 'EURUSD_GMT+0_NO-DST1.csv'
-data = pd.read_csv(filename, delimiter=',',skiprows=0)
+##filename = 'EURUSD_GMT+0_NO-DST1.csv'
+filename = 'EURUSD_GMT_ts_bid_ask_5-4-02--9-9-19.csv' # this is like 11 GB so be careful
+
+##data = pd.read_csv(filename, delimiter=',',skiprows=0)
 
 # to deal with me having add a header once...
-try: ts = data['Date'].str.split(" ",n=1, expand=True)
-except KeyError:
-    data = pd.read_csv(filename, delimiter=',',skiprows=0,names=['Date','Bid','Ask'])
-    ts = data['Date'].str.split(" ",n=1, expand=True)
+##try: ts = data['Date'].str.split(" ",n=1, expand=True)
+##except KeyError:
+##    data = pd.read_csv(filename, delimiter=',',skiprows=0,names=['Date','Bid','Ask'])
+##    ts = data['Date'].str.split(" ",n=1, expand=True)
     
-data['Day']=ts[0]
-data['Time']=ts[1]
-data.drop(columns=['Date'],inplace=True)
-data['Day'] = pd.to_datetime(data['Day'],yearfirst=True) # parse days into date time frmt
-
-ts_day = data['Day']
-bid = data['Bid']
-ask = data['Ask']
+##data['Day']=ts[0]
+##data['Time']=ts[1]
+##data.drop(columns=['Date'],inplace=True)
+##data['Day'] = pd.to_datetime(data['Day'],yearfirst=True) # parse days into date time frmt
+##
+##ts_day = data['Day']
+##bid = data['Bid']
+##ask = data['Ask']
 
 ##rng = pd.date_range(datetime.date(2015,1,1),datetime.date(2015,7,31))
 
@@ -38,7 +40,7 @@ ask = data['Ask']
 ##plt.plot(range(len(bid.values)),bid.values)
 ##plt.show()
 
-ticks_per_day = data['Day'].value_counts()
+##ticks_per_day = data['Day'].value_counts()
 
 ##fig, ax = plt.subplots()
 
@@ -53,6 +55,21 @@ ticks_per_day = data['Day'].value_counts()
 ##plt.show()
 
 
+##chunksize = 10 ** 8
+chunksize = 1000
+c = 0
+timer = dt.datetime.now()
+# each chunk really is a new dataframe
+for chunk in pd.read_csv(filename, chunksize=chunksize, names=['Date','Bid','Ask']):
+    ems = (dt.datetime.now() - timer).microseconds
+    timer = dt.datetime.now()
+    print('micros:',ems)
+    print(' ')
+##    print('chunk ',c)
+##    print(chunk)
+##    process(chunk)
+    if c > 2: break
+    c += 1
 
 
-
+##df.to_csv('my_csv.csv', mode='a', header=False)
